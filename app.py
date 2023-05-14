58942837395ea2db0c2f4f5a2097a39aecd79ab9
Hans <ahacicm02@gmail.com>
2023-05-14 08:18:21 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,42 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sensor_data.db'
 db = SQLAlchemy(app)
 
-class SenzorData(db.Model):
+class SensorData(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     temperatura = db.Column(db.Float)
-    vlaznost = db.Column(db.Float)
+    vlaga = db.Column(db.Float)
     svetlost = db.Column(db.Float)
-    glasnost = db.Column(db.Float)
-    CO = db.Column(db.Float)
+    GLasnost = db.Column(db.Float)
 
 @app.route('/save-sensor-data', methods=['POST'])
-def shrani_podatke():
+def save_sensor_data():
     data = request.get_json()
-    sensor_data = SenzorData(
-        temperatura=data['temperatura'],
-        vlaznost=data['vlaznost'],
-        glasnost=data['glasnost'],
-        svetlost=data['svetlost'],
-        CO=data['CO']
+    sensor_data = SensorData(
+        temperaturq=data['temperature'],
+        vlaga=data['humidity'],
+        svetlost=data['light_intensity'],
+        glasnost=data['carbon_monoxide']
     )
     db.session.add(sensor_data)
     db.session.commit()
     return 'Data saved successfully!'
+
+@app.route('/get-sensor-data', methods=['GET'])
+def get_sensor_data():
+    sensor_data = SensorData.query.all()
+    data = []
+    for row in sensor_data:
+        data.append({
+            'temperatura': row.temperatura,
+            'vlaga': row.vlaga,
+            'svetlsot': row.svetlsot,
+            'glasnost': row.glasnost
+        })
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
