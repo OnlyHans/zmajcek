06186d9db2c84125c6859d86eb6a2254a5258294
Hans <ahacicm02@gmail.com>
2023-05-14 09:48:32 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,42 +1,40 @@
-from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, request, render_template
+
+import sqlite3
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sensor_data.db'
-db = SQLAlchemy(app)
+app.config['DEBUG'] = True
+app.config['DATABASE'] = 'sensor_data.db'
 
-class SensorData(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    temperatura = db.Column(db.Float)
-    vlaga = db.Column(db.Float)
-    svetlost = db.Column(db.Float)
-    GLasnost = db.Column(db.Float)
+def get_db():
+    """Connect to the SQLite database."""
+    db = sqlite3.connect(app.config['DATABASE'])
+    db.row_factory = sqlite3.Row
+    return db
+
+def init_db():
+    """Create the database tables."""
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
 
 @app.route('/save-sensor-data', methods=['POST'])
 def save_sensor_data():
     data = request.get_json()
-    sensor_data = SensorData(
-        temperaturq=data['temperature'],
-        vlaga=data['humidity'],
-        svetlost=data['light_intensity'],
-        glasnost=data['carbon_monoxide']
-    )
-    db.session.add(sensor_data)
-    db.session.commit()
+    db = get_db()
+    db.execute('INSERT INTO SensorData (temperature, humidity, light_intensity, carbon_monoxide) VALUES (?, ?, ?, ?)', (data['temperature'], data['humidity'], data['light_intensity'], data['carbon_monoxide']))
+    db.commit()
     return 'Data saved successfully!'
 
-@app.route('/get-sensor-data', methods=['GET'])
-def get_sensor_data():
-    sensor_data = SensorData.query.all()
-    data = []
-    for row in sensor_data:
-        data.append({
-            'temperatura': row.temperatura,
-            'vlaga': row.vlaga,
-            'svetlsot': row.svetlsot,
-            'glasnost': row.glasnost
-        })
-    return jsonify(data)
+@app.route('/view-sensor-data', methods=['GET'])
+def view_sensor_data():
+    db = get_db()
+    cursor = db.execute('SELECT * FROM SensorData ORDER BY id DESC')
+    rows = cursor.fetchall()
+    return render_template('sensor_data.html', rows=rows)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    init_db()
+    app.run()
